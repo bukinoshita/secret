0b1f11a87777c82236c106fded6c0bec4cc85a92
Bu Kinoshita <bukinoshita@gmail.com>
2019-03-04 14:54:48 -0300

    patch: add server

--- server.js
+++ server.js
@@ -1,33 +1,21 @@
-'use strict'
-
-const { createServer } = require('http')
-const { parse } = require('url')
+// Packages
+const express = require('express')
 const next = require('next')
-const pathMatch = require('path-match')
+
+// Routes
+const routes = require('./routes')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler()
-const route = pathMatch()
-const matchS = route('/s/:id')
-const matchC = route('/c/:id')
+const handler = routes.getRequestHandler(app)
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const { pathname, query } = parse(req.url, true)
+  express()
+    .use(handler)
+    .listen(port, err => {
+      if (err) throw err
 
-    if (matchC(pathname)) {
-      return app.render(req, res, '/c', Object.assign(matchC(pathname), query))
-    }
-
-    if (matchS(pathname)) {
-      return app.render(req, res, '/s', Object.assign(matchS(pathname), query))
-    }
-
-    handle(req, res)
-  }).listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
+      console.log(`> Ready on http://localhost:${port}`)
+    })
 })
