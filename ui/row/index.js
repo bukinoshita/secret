4410b9e4824edf6f60238e08259608f9f9f03744
Bu Kinoshita <bukinoshita@gmail.com>
2019-03-04 16:53:34 -0300

    patch: change row to pure component

--- ui/row/index.js
+++ ui/row/index.js
@@ -1,27 +1,31 @@
 // Packages
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
 // UI
 import theme from '../theme'
 
-const Row = ({ children }) => {
-  return (
-    <div>
-      {children}
+class Row extends PureComponent {
+  render() {
+    const { children } = this.props
 
-      <style jsx>{`
-        div {
-          width: 100%;
-          max-width: 750px;
-          padding-left: ${theme.spacing.large};
-          padding-right: ${theme.spacing.large};
-          margin-left: auto;
-          margin-right: auto;
-        }
-      `}</style>
-    </div>
-  )
+    return (
+      <div>
+        {children}
+
+        <style jsx>{`
+          div {
+            width: 100%;
+            max-width: 750px;
+            padding-left: ${theme.spacing.large};
+            padding-right: ${theme.spacing.large};
+            margin-left: auto;
+            margin-right: auto;
+          }
+        `}</style>
+      </div>
+    )
+  }
 }
 
 Row.propTypes = {
