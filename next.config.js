c1b69406acbdb0d1f57c97640be8e56cba0c08ba
Bu Kinoshita <bukinoshita@gmail.com>
2019-03-04 16:47:14 -0300

    patch: add new env vars

--- next.config.js
+++ next.config.js
@@ -6,5 +6,9 @@ require('dotenv').config()
 module.exports = {
   poweredByHeader: false,
   generateBuildId: async () => pkg.version,
-  publicRuntimeConfig: { API_URL: process.env.API_URL }
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL,
+    SECRET_TOKEN: process.env.SECRET_TOKEN,
+    ACCESS_TOKEN: process.env.ACCESS_TOKEN
+  }
 }
