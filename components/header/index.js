5b8bd1afa68b2cef1f59ec9137194ffc2b9d09cb
Bu Kinoshita <bukinoshita@gmail.com>
2019-03-05 13:01:00 -0300

    patch: header new ui

--- components/header/index.js
+++ components/header/index.js
@@ -1,39 +1,28 @@
 // Packages
 import React, { PureComponent } from 'react'
+import Link from 'next/link'
 
-// Root
-import pkg from '../../package'
-
-// UI
-import theme from '../../ui/theme'
+// Components
+import Logo from '../logo'
 
 class Header extends PureComponent {
   render() {
     return (
       <header>
-        <div>
-          <h1>{pkg.name}</h1>
-          <h2>{pkg.description}.</h2>
-        </div>
+        <Link href="/" prefetch>
+          <a>
+            <Logo />
+          </a>
+        </Link>
 
         <style jsx>{`
           header {
-            height: 300px;
-            align-items: center;
             display: flex;
-            margin-bottom: 100px;
-          }
-
-          h1 {
-            font-size: ${theme.typography.title.fontSize};
-            font-weight: ${theme.typography.title.fontWeight};
-            line-height: ${theme.typography.title.lineHeight};
-          }
-
-          h2 {
-            font-size: ${theme.typography.h400.fontSize};
-            font-weight: ${theme.typography.h400.fontWeight};
-            line-height: ${theme.typography.h400.lineHeight};
+            justify-content: space-between;
+            height: 200px;
+            align-items: center;
+            padding-left: 150px;
+            padding-right: 150px;
           }
         `}</style>
       </header>
