5f9302e47612b30001bf0ff77bd1a565ca334782
Bu Kinoshita <bukinoshita@gmail.com>
2019-03-05 15:50:33 -0300

    patch: add next offline to next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,38 @@
+// Packages
+const withOffline = require('next-offline')
+
 // Root
 const pkg = require('./package')
 
 require('dotenv').config()
 
-module.exports = {
+module.exports = withOffline({
   target: 'serverless',
   poweredByHeader: false,
   generateBuildId: async () => pkg.version,
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'networkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  },
   env: {
     apiUrl: process.env.API_URL,
     secretToken: process.env.SECRET_TOKEN,
     accessToken: process.env.ACCESS_TOKEN
   }
-}
+})
