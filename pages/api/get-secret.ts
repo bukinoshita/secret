f9cc34f622352f1b630ab1495710108cefab6942
bukinoshita <bukinoshita@gmail.com>
2020-03-12 01:20:01 -0300

    fix: minor tweaks

--- pages/api/get-secret.ts
+++ pages/api/get-secret.ts
@@ -2,11 +2,14 @@ import { NowRequest, NowResponse } from '@now/node'
 
 import { api } from '../../utils/api'
 
-export default async (request: NowRequest, res: NowResponse): Promise<void> => {
+export default async (
+  request: NowRequest,
+  response: NowResponse
+): Promise<void> => {
   const { id } = request.query
   const {
     data: { iv, cipherText }
   } = await api.get(`http://localhost:3000/api/secret/get?id=${id}`)
 
-  res.status(201).send({ iv, cipherText })
+  response.status(201).send({ iv, cipherText })
 }
