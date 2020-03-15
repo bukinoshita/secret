1eb5cfb2ea6714bd9cbff65158102dcb1853fc57
bukinoshita <bukinoshita@gmail.com>
2020-03-15 13:39:25 -0300

    feat: add icon check

---
+++ ui/icon/icon-check.tsx
@@ -0,0 +1,17 @@
+import { PureComponent } from 'react'
+
+import { IconBase } from './icon-base'
+
+import { IconProps } from './icon.types'
+
+export class IconCheck extends PureComponent<IconProps> {
+  render() {
+    const { className, onClick, style } = this.props
+
+    return (
+      <IconBase className={className} style={style} onClick={onClick}>
+        <polyline points="20 6 9 17 4 12" />
+      </IconBase>
+    )
+  }
+}
