6e1972ead74cb61ff870fa98bd606c8fa9a99de0
bukinoshita <bukinoshita@gmail.com>
2020-03-17 17:03:59 -0300

    feat: add page title

---
+++ components/page-title/page-title.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import { space, Weight } from 'ui/theme'
+
+import { PageTitleProps } from './page-title.types'
+
+export const PageTitle = ({ title, subtitle, center }: PageTitleProps) => {
+  return (
+    <section>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+
+      <style jsx={true}>{`
+        h1 {
+          font-size: 30px;
+          font-weight: ${Weight.Bold};
+          text-transform: capitalize;
+          text-align: ${center ? 'center' : 'left'};
+        }
+
+        h2 {
+          margin-top: ${space.spacing(2)};
+          margin-bottom: ${space.spacing(9)};
+          font-weight: ${Weight.Regular};
+          font-size: 16px;
+          line-height: 26px;
+          max-width: 400px;
+          text-align: ${center ? 'center' : 'left'};
+          margin-left: ${center ? 'auto' : 'initial'};
+          margin-right: ${center ? 'auto' : 'initial'};
+        }
+      `}</style>
+    </section>
+  )
+}
+
+PageTitle.defaultProps = {
+  center: false
+}
---
+++ components/page-title/page-title.types.ts
@@ -0,0 +1,5 @@
+export interface PageTitleProps {
+  title: string
+  subtitle: string
+  center?: boolean
+}
