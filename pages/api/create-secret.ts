eb6d94cca2cc2b965d18dc5d8fdbc0f91975d8d4
bukinoshita <bukinoshita@gmail.com>
2020-03-17 17:04:22 -0300

    refactor: mock api

--- pages/api/create-secret.ts
+++ pages/api/create-secret.ts
@@ -1,12 +1,10 @@
 import { NowRequest, NowResponse } from '@now/node'
 
-import { api } from '../../utils/api'
+export default async (_req: NowRequest, res: NowResponse) => {
+  // const { iv, cipherText } = req.body
+  // const {
+  //   data: { id }
+  // } = await api.post('http://localhost:3000/api/secret/create', { iv, cipherText })
 
-export default async (req: NowRequest, res: NowResponse) => {
-  const { iv, cipherText } = req.body
-  const {
-    data: { id }
-  } = await api.post('http://localhost:3000/api/secret/create', { iv, cipherText })
-
-  res.status(201).send({ id })
+  res.status(201).send({ id: 'ekjweniew93enkjdnw2nksa' })
 }
